9886494ab7b2cfe674a1876de09c49ade101b0e3
W-OV <williamoverall@icloud.com>
2025-04-23 15:41:29 +0100

    expanded view added

--- lib/src/eventsView.dart
+++ lib/src/eventsView.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:socdrawer/src/eventsViewExpanded.dart';
 import 'event.dart';
 
 class EventsView extends StatelessWidget {
   final List<Event> events = [
     Event(
       name: 'Purple Wednesday',
-      description: 'description.. blah blah blah',
+      description: 'description.. blah blah blah \n more text \n even more text',
       dateTime: DateTime.now(),
-      location: 'location',
+      location: 'one eyed dog portsmouth',
       society: 'society its in',
     ),
   ];
@@ -25,15 +26,22 @@ class EventsView extends StatelessWidget {
               itemBuilder: (context, index) {
                 final event = events[index];
                 return Card(
-                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   child: ListTile(
                     title: Text(event.name),
-                    subtitle: Text('${event.dateTime.toLocal()}\n${event.location}\n${event.society}${event.description}'),
+                    subtitle: Text(
+                        '${event.dateTime.toLocal()}\n${event.location}\n${event.society}'),
                     isThreeLine: true,
                     trailing: const Icon(Icons.arrow_forward),
                     onTap: () {
-
                       // --- OPEN EVENT DETAILS ---
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EventsViewExpanded(event: event),
+                        ),
+                      );
                       
                     },
                   ),
