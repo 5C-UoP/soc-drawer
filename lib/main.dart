1968fc6f75720a18ac5ed8f0c333ae565aeb62d0
unknown <wadafareed@gmail.com>
2025-05-11 16:21:41 +0100

    initial commit with project files

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:socdrawer/src/app.dart';
 import 'src/controllers/settings_controller.dart';
 import 'src/views/settings/settings_service.dart';
 
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_core/flutter_chat_core.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
@@ -21,4 +27,51 @@ void main() async {
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(SocDrawer(settingsController: settingsController));
+}
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_core/flutter_chat_core.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+class Basic extends StatefulWidget {
+  const Basic({super.key});
+
+  @override
+  BasicState createState() => BasicState();
+}
+
+class BasicState extends State<Basic> {
+  final _chatController = InMemoryChatController();
+
+  @override
+  void dispose() {
+    _chatController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Chat(
+        chatController: _chatController,
+        currentUserId: 'user1',
+        onMessageSend: (text) {
+          _chatController.insertMessage(
+            TextMessage(
+              // Better to use UUID or similar for the ID - IDs must be unique
+              id: '${Random().nextInt(1000) + 1}',
+              authorId: 'user1',
+              createdAt: DateTime.now().toUtc(),
+              text: text,
+            ),
+          );
+        },
+        resolveUser: (UserID id) async {
+          return User(id: id, name: 'Farid');
+        },
+      ),
+    );
+  }
 }
