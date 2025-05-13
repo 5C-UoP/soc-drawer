8f5b9fbc2551a93e0671395e55a69642f09cce76
sevmern <up2246633@myport.ac.uk>
2025-05-13 19:41:14 +0100

    Added more tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,7 @@ import 'package:socdrawer/main.dart' as app;
 import 'package:socdrawer/src/controllers/society_controller.dart';
 import 'package:socdrawer/src/models/event.dart';
 import 'package:socdrawer/src/views/components/event_card.dart' as event;
+import 'package:socdrawer/src/views/events/events_view.dart';
 
 void main() {
   testWidgets('Renders Login screen', (WidgetTester tester) async {
@@ -28,6 +29,24 @@ void main() {
     expect(find.textContaining('Login'), findsWidgets);
   });
 
+  // test valid login
+  testWidgets(
+    "Checks correct login",
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Log in if needed
+      await tester.enterText(
+          find.byType(TextField).at(0), 'admin@myport.ac.uk');
+      await tester.enterText(find.byType(TextField).at(1), 'admin');
+      await tester.tap(find.text('Login'));
+      await tester.pumpAndSettle();
+
+      expect(find.textContaining('My Calendar'), findsWidgets);
+    },
+  );
+
   // Tests to make sure that our event card works
   testWidgets('Visible Events', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
@@ -48,5 +67,24 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.textContaining('Paid event'), findsWidgets);
+  });
+
+  testWidgets('navigates to EventsView when "Events" is tapped',
+      (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    // Log in if needed
+    await tester.enterText(find.byType(TextField).at(0), 'admin@myport.ac.uk');
+    await tester.enterText(find.byType(TextField).at(1), 'admin');
+    await tester.tap(find.text('Login'));
+    await tester.pumpAndSettle();
+
+    // there is no events by defualt
+    await tester.tap(find.text('Events'));
+    await tester.pumpAndSettle();
+
+    // Verify navigation to EventsView
+    expect(find.byType(EventsView), findsOneWidget);
   });
 }
