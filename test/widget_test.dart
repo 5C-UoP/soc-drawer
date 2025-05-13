cbfebb859f46a60fee820bd53fd07477a5c43f8e
TheNat_ <74879098+ParallexTheNat@users.noreply.github.com>
2025-05-13 17:35:46 +0100

    testing implemented

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,31 +1,26 @@
-// This is an example Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-//
-// Visit https://flutter.dev/to/widget-testing for
-// more information about Widget testing.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:socdrawer/src/views/components/bottom_navbar.dart' as bottom_navbar;
+import 'package:socdrawer/src/views/components/event_card.dart' as event;
+import 'package:socdrawer/main.dart' as app;
 
 void main() {
-  group('MyWidget', () {
-    testWidgets('should display a string of text', (WidgetTester tester) async {
-      // Define a Widget
-      const myWidget = MaterialApp(
-        home: Scaffold(
-          body: Text('Hello'),
-        ),
-      );
+  testWidgets('Renders default screen', (WidgetTester tester) async {
+    app.main(); // this runs runApp
+    await tester.pumpAndSettle();
 
-      // Build myWidget and trigger a frame.
-      await tester.pumpWidget(myWidget);
+    expect(find.textContaining('Society'), findsWidgets); // Adjust based on your UI
+  });
+    testWidgets('Checks navbar', (WidgetTester tester) async {
+    bottom_navbar.BottomNavBar(); // this runs runApp
+    await tester.pumpAndSettle();
 
-      // Verify myWidget shows some text
-      expect(find.byType(Text), findsOneWidget);
-    });
+    expect(find.textContaining('Home'), findsWidgets); // Adjust based on your UI
+  });
+    testWidgets('Visible Events', (WidgetTester tester) async {
+    event.EventCard(); // this runs runApp
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('Paid event'), findsWidgets); // Adjust based on your UI
   });
 }
